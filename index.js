241fb970269d06d679ed1212ed204f4de0c48667
Fig Yogurt <itisaftab@gmail.com>
2022-09-08 15:26:05 -0400

    refactor(app): :hammer: cleaned up the code a bit

--- index.js
+++ index.js
@@ -34,9 +34,8 @@ const updateTheLogs = async (arr) => {
   const deviceDetector = new DeviceDetector();
   for (let i = 0; i < arr.length; i++) {
     const oneLog = arr[i];
-    let updatedLog = oneLog;
-    const obj = {};
-    obj.log = oneLog;
+    const logInfo = {};
+    logInfo.log = oneLog;
 
     try {
       const spaceSplit = oneLog.split(" ");
@@ -48,11 +47,9 @@ const updateTheLogs = async (arr) => {
         geoLocation.country.names &&
         geoLocation.country.names.en
       ) {
-        updatedLog += ` Country: ${geoLocation.country.names.en}; `;
-        obj.country = geoLocation.country.names.en;
+        logInfo.country = geoLocation.country.names.en;
       } else {
-        updatedLog += ` Country: Unknown; `;
-        obj.country = "Unknown";
+        logInfo.country = "Unknown";
       }
       if (
         geoLocation.subdivisions &&
@@ -60,11 +57,9 @@ const updateTheLogs = async (arr) => {
         geoLocation.subdivisions[0].names &&
         geoLocation.subdivisions[0].names.en
       ) {
-        updatedLog += ` State: ${geoLocation.subdivisions[0].names.en}; `;
-        obj.state = geoLocation.subdivisions[0].names.en;
+        logInfo.state = geoLocation.subdivisions[0].names.en;
       } else {
-        updatedLog += ` State: Unknown; `;
-        obj.state = "Unknown";
+        logInfo.state = "Unknown";
       }
     } catch (error) {
       console.log(error);
@@ -75,43 +70,44 @@ const updateTheLogs = async (arr) => {
       const userInfo = deviceDetector.parse(quoteSplit[5]);
 
       if (userInfo.device && userInfo.device.type) {
-        updatedLog += ` DeviceType: ${userInfo.device.type} `;
-        obj.device = userInfo.device.type;
+        logInfo.device = userInfo.device.type;
       } else {
-        updatedLog += ` DeviceType: Unknown `;
-        obj.device = "Unknown";
+        logInfo.device = "Unknown";
       }
 
       if (userInfo.client && userInfo.client.name) {
-        updatedLog += ` Browser: ${userInfo.client.name} `;
-        obj.browser = userInfo.client.name;
+        logInfo.browser = userInfo.client.name;
       } else {
-        updatedLog += ` Browser: Unknown `;
-        obj.browser = "Unknown";
+        logInfo.browser = "Unknown";
       }
     } catch (error) {
       console.log(error);
     }
-    result.push(obj);
+    result.push(logInfo);
   }
   return result;
 };
 
 const makeCSV = async (cleanerLogs) => {
-  const headerCSV = ["country", "state", "device", "browser"];
-  const convertCSV = [
-    headerCSV,
-    ...cleanerLogs.map((log) => [
-      log.country,
-      log.state,
-      log.device,
-      log.browser,
-    ]),
-  ]
-    .map((el) => el.join(","))
-    .join("\n");
-  const output = await fs.writeFile("output.csv", convertCSV);
-  return output;
+  try {
+    const headerCSV = ["country", "state", "device", "browser"];
+    const convertCSV = [
+      headerCSV,
+      ...cleanerLogs.map((log) => [
+        log.country,
+        log.state,
+        log.device,
+        log.browser,
+      ]),
+    ]
+      .map((el) => el.join(","))
+      .join("\n");
+    const output = await fs.writeFile("output.csv", convertCSV);
+    console.log("DONE!");
+    return output;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 runIt();
