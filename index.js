c17cb49c9b55b635b766b45e47c6f5d72311bf69
Fig Yogurt <itisaftab@gmail.com>
2022-09-08 15:08:07 -0400

    feat(app): :tada: initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const fs = require("fs").promises;
+const { Reader } = require("@maxmind/geoip2-node");
+const DeviceDetector = require("device-detector-js");
+
+const fileName = "gobankingrates.com.access.short.log"; // change your file here
+
+const runIt = async () => {
+  const arr = await getFileInfo(fileName);
+  if (!arr) {
+    return "error occured during read file";
+  }
+  const cleanerLogs = await updateTheLogs(arr);
+  await makeCSV(cleanerLogs);
+};
+
+const getFileInfo = async (file) => {
+  try {
+    const data = await fs.readFile(file, "utf8");
+    const arr = data.split(/\r?\n|\r|\n/g);
+    return arr;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getLocationInfo = async (ip) => {
+  const reader = await Reader.open("GeoLite2-City.mmdb");
+  const response = await reader.city(ip);
+  return response;
+};
+
+const updateTheLogs = async (arr) => {
+  const result = [];
+  const deviceDetector = new DeviceDetector();
+  for (let i = 0; i < arr.length; i++) {
+    const oneLog = arr[i];
+    let updatedLog = oneLog;
+    const obj = {};
+    obj.log = oneLog;
+
+    try {
+      const spaceSplit = oneLog.split(" ");
+      const iPAddress = spaceSplit[0].trim(); // location is in the first index it seems; if I had more freetime; id probably use a regex to handle all the scnarios instead of splitting by space or double quotes
+      const geoLocation = await getLocationInfo(iPAddress);
+
+      if (
+        geoLocation.country &&
+        geoLocation.country.names &&
+        geoLocation.country.names.en
+      ) {
+        updatedLog += ` Country: ${geoLocation.country.names.en}; `;
+        obj.country = geoLocation.country.names.en;
+      } else {
+        updatedLog += ` Country: Unknown; `;
+        obj.country = "Unknown";
+      }
+      if (
+        geoLocation.subdivisions &&
+        geoLocation.subdivisions[0] &&
+        geoLocation.subdivisions[0].names &&
+        geoLocation.subdivisions[0].names.en
+      ) {
+        updatedLog += ` State: ${geoLocation.subdivisions[0].names.en}; `;
+        obj.state = geoLocation.subdivisions[0].names.en;
+      } else {
+        updatedLog += ` State: Unknown; `;
+        obj.state = "Unknown";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      const quoteSplit = oneLog.split(`"`);
+      const userInfo = deviceDetector.parse(quoteSplit[5]);
+
+      if (userInfo.device && userInfo.device.type) {
+        updatedLog += ` DeviceType: ${userInfo.device.type} `;
+        obj.device = userInfo.device.type;
+      } else {
+        updatedLog += ` DeviceType: Unknown `;
+        obj.device = "Unknown";
+      }
+
+      if (userInfo.client && userInfo.client.name) {
+        updatedLog += ` Browser: ${userInfo.client.name} `;
+        obj.browser = userInfo.client.name;
+      } else {
+        updatedLog += ` Browser: Unknown `;
+        obj.browser = "Unknown";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    result.push(obj);
+  }
+  return result;
+};
+
+const makeCSV = async (cleanerLogs) => {
+  const headerCSV = ["country", "state", "device", "browser"];
+  const convertCSV = [
+    headerCSV,
+    ...cleanerLogs.map((log) => [
+      log.country,
+      log.state,
+      log.device,
+      log.browser,
+    ]),
+  ]
+    .map((el) => el.join(","))
+    .join("\n");
+  const output = await fs.writeFile("output.csv", convertCSV);
+  return output;
+};
+
+runIt();
